96db0039ff7e6aed7b560ad40c6afdcde9b35126
sohan1997i <sohan1997i@gmail.com>
2021-05-04 15:43:08 +0600

    post app created

--- blood_lagbe/settings.py
+++ blood_lagbe/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages',
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blood_lagbe/urls.py
+++ blood_lagbe/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 
     # pages url ----
 
